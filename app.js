0ec784970c99332ccb061dda1ef804d90654708e
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2017-06-07 10:34:19 +0000

    Add Posts Todos Route and Refactor Mongoose

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const {mongoose} = require('./db/mongoose');
+const {User} = require('./models/user');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+/*let newTodo = new Todo({
+  text: 'Cook dinner'
+});
 
-var app = express();
+newTodo.save().then( doc => {
+  console.log('Saved todo', doc);
+}, e => {
+  console.log('Unable to save');
+});
+
+let user = new User({
+  email: 'y@example.com '
+});
+
+user.save().then( doc => {
+  console.log('User saved', doc);
+}, e => {
+    console.log('Unable to save user', e);
+});*/
+
+
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,9 +49,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {Todo} = require('../models/todo');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/todos', (req, res, next) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+  todo.save()
+    .then( doc => {
+      res.send(doc);
+  }, e => {
+      res.status(400).send(e);
+  });
 });
 
 module.exports = router;
